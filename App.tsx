05ec375ef59d61dbb2c63ee79f86821d816b168b
Kaustubh Patil <patilkaustubh990@gmail.com>
2025-01-11 00:00:42 +0530

    FlatList and rendering dummy data (EZ)

--- App.tsx
+++ App.tsx
@@ -1,151 +1,151 @@
 import React from 'react';
-import { StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+
+const dummyData = [
+  // const dummyData = [
+    {
+      id: 1,
+      name: 'Siddhi Hingmire',
+      email: 'johndoe@mail.com',
+      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 2,
+      name: 'Kaustubh Kulkarni',
+      email: 'kkarni@mail.com',
+      image: 'https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 3,
+      name: 'Aman Kamrakar',
+      email: 'kamaman@mail.com',
+      image: 'https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 4,
+      name: 'Aditya Sharma',
+      email: 'aditya.sharma@mail.com',
+      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 5,
+      name: 'Ravi Verma',
+      email: 'ravi.verma@mail.com',
+      image: 'https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 6,
+      name: 'Yash Rajput',
+      email: 'yash.rajput@mail.com',
+      image: 'https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 7,
+      name: 'Nikhil Patil',
+      email: 'nikhil.patil@mail.com',
+      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 8,
+      name: 'Vikas Jadhav',
+      email: 'vikas.jadhav@mail.com',
+      image: 'https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 9,
+      name: 'Rohan Deshmukh',
+      email: 'rohan.deshmukh@mail.com',
+      image: 'https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 10,
+      name: 'Sahil Mehta',
+      email: 'sahil.mehta@mail.com',
+      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 11,
+      name: 'Kunal Pandey',
+      email: 'kunal.pandey@mail.com',
+      image: 'https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 12,
+      name: 'Aniket Joshi',
+      email: 'aniket.joshi@mail.com',
+      image: 'https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 13,
+      name: 'Harshavardhan Kulkarni',
+      email: 'harsh.kulkarni@mail.com',
+      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    }  
+];
 
 const App = () => {
-  const theme = useColorScheme();
-  const darkMode = theme ==='dark';
-  const backgroundColor = darkMode ? "black": "white";
-  const textColor = darkMode ? 'white' :" black";
   return (
-    <View style={[styles.container, {backgroundColor: backgroundColor}]}>
-      {/* <Text style={[styles.textColor,{color: textColor}]}>Hello App</Text> */}
-      <View style={styles.view2}>
-        {/* <Text style={[styles.textColor,{color: textColor}]}>View 2</Text> */}
-      </View>    
-      <View style={styles.view3}>
-        {/* <Text style={[styles.textColor,{color: textColor}]}>View 3</Text> */}
-        </View>  
-        <View style={styles.view4}>
-        {/* <Text style={[styles.textColor,{color: textColor}]}>View 3</Text> */}
-        </View>  
+    <View style={styles.container}>
+      <FlatList
+        data={dummyData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image
+              style={styles.image}
+              source={{ uri: item.image }}
+            />
+            <Text style={styles.nameText}>{item.name}</Text>
+            <Text style={styles.emailText}>{item.email}</Text>
+          </View>
+        )}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
     </View>
   );
-}
-// flex logic:
-// 1. 1st view is 1/6 of the screen
-// 2. 2nd view is 2/6 of the screen
-// 3. 3rd view is 3/6 of the screen
-
-// flex logic for equal division:
-// 1. 1st view is 1/6 of the screen
-// 2. 2nd view is 1/6 of the screen
-// 3. 3rd view is 1/6 of the screen
+};
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    // width:"100%",
-    // height:"100%",
-    // justifyContent:"center",
-    // alignItems:"center"
-
-    flexDirection:"column", //main axis
-    // flex-start, flex-end, center, space-between, space-around, space-evenly
-    justifyContent:"center", //cross axis. Aligns items horizontally */
-    alignItems:"center", //cross axis. Aligns items vertically */
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
   },
-  textColor:{
-    fontSize: 60,
-    fontWeight: "800",
-   
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 15,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginHorizontal: 10,
   },
-  view2:{
-    // flex:1,
-    backgroundColor:"green",
-    width:50,
-    height:50,
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginBottom: 10,
   },
-  view3:{
-    // flex:1,
-    backgroundColor:"skyblue",
-    width:50,
-    height:50,
+  nameText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
   },
-  view4:{
-    // flex:1,
-    backgroundColor:"orange",
-    width:50,
-    height:50,
-  }
-})
+  emailText: {
+    fontSize: 14,
+    color: '#777',
+    marginTop: 5,
+  },
+  separator: {
+    height: 15,
+  },
+});
 
 export default App;
-
-
-
-
-// import { Button, Image, StyleSheet, Text, Touchable, TouchableOpacity, View, Alert, Pressable, SafeAreaView, useColorScheme } from 'react-native'
-// import React from 'react'
-
-// const App = () => {
-//  const theme = useColorScheme();
-//  console.log(theme, "theme")
-
-//  const isDarkMode = theme ==='dark';
-//  const backgroundColor = isDarkMode ? "black" : "white";
-//  const textColor = isDarkMode ? "white" : "black";
-//   return (
-//     <View style={[styles.container, {backgroundColor: backgroundColor}]}>
-//       <Text style={[styles.text, {color: textColor}]}>Hello App</Text>
-//     </View>
-//   )
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1, // Makes the container take up all available space
-//     // backgroundColor: 'dark',
-//     justifyContent: 'center', // Centers children vertically 
-//     alignItems: 'center', // Centers children horizontally
-//   },
-//   text: {
-//     color: 'black',
-//     fontSize: 30,
-//     fontWeight: '800'
-//   }
-// });
-// const btnStyles = StyleSheet.create({
-//   container: {
-//     padding: 20,
-//     paddingTop: 10,
-//     backgroundColor: "skyblue",
-//     borderRadius: 5,
-//     margin: 10
-//   }
-// })
-// export default App
-
-// <SafeAreaView style={styles.container}>
-//   {/* <View style={styles.container}> */}
-//     <Text style={{color:"red", fontSize:30, fontWeight:"800"}}>App21</Text>
-//     <Image
-//       source={{ uri: "https://plus.unsplash.com/premium_photo-1678566111481-8e275550b700?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
-//       style={{
-//         width: 200,
-//         height: 200,
-//         borderRadius: 100,
-//         margin: 10,
-//       }}
-//     />
-//     <Button title='Just Exploring ReactNative' />
-
-//     {/* <TouchableOpacity 
-//   style={btnStyles.container}
-//   onPress={() => Alert.alert('This is not a normal btn what it does is basically provide a touchable effect')}>
-//     <Text>onPress btn</Text>
-//     </TouchableOpacity>
-    
-//     <TouchableOpacity 
-//     style={{...btnStyles.container,backgroundColor:"red",}}
-//     onLongPress={() => Alert.alert('This is not a normal btn what it does is basically provide a touchable effect of longPress}')}>
-//     <Text>onLongPress btn</Text>
-//     </TouchableOpacity> */}
-//     <Pressable
-//       style={btnStyles.container}
-//       onPress={() => Alert.alert('This is not a normal btn what it does is basically provide a touchable effect')}
-//     >
-//       <Text>Pressable btn</Text>
-//     </Pressable>
-//   {/* </View> */}
-
-// </SafeAreaView>

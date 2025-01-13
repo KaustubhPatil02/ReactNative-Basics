68ef8e15485df61e56550a496f3d3ec0145fb6e0
Kaustubh Patil <patilkaustubh990@gmail.com>
2025-01-13 15:32:42 +0530

    Refactor App component to implement a login interface, replacing the FlatList with TextInput fields and a login button

--- App.tsx
+++ App.tsx
@@ -1,154 +1,49 @@
-import React from 'react';
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
-
-const dummyData = [
-  // const dummyData = [
-    {
-      id: 1,
-      name: 'Siddhi Hingmire',
-      email: 'johndoe@mail.com',
-      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 2,
-      name: 'Kaustubh Kulkarni',
-      email: 'kkarni@mail.com',
-      image: 'https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 3,
-      name: 'Aman Kamrakar',
-      email: 'kamaman@mail.com',
-      image: 'https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 4,
-      name: 'Aditya Sharma',
-      email: 'aditya.sharma@mail.com',
-      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 5,
-      name: 'Ravi Verma',
-      email: 'ravi.verma@mail.com',
-      image: 'https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 6,
-      name: 'Yash Rajput',
-      email: 'yash.rajput@mail.com',
-      image: 'https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 7,
-      name: 'Nikhil Patil',
-      email: 'nikhil.patil@mail.com',
-      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 8,
-      name: 'Vikas Jadhav',
-      email: 'vikas.jadhav@mail.com',
-      image: 'https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 9,
-      name: 'Rohan Deshmukh',
-      email: 'rohan.deshmukh@mail.com',
-      image: 'https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 10,
-      name: 'Sahil Mehta',
-      email: 'sahil.mehta@mail.com',
-      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 11,
-      name: 'Kunal Pandey',
-      email: 'kunal.pandey@mail.com',
-      image: 'https://images.unsplash.com/photo-1640960543409-dbe56ccc30e2?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 12,
-      name: 'Aniket Joshi',
-      email: 'aniket.joshi@mail.com',
-      image: 'https://images.unsplash.com/photo-1640951613773-54706e06851d?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    },
-    {
-      id: 13,
-      name: 'Harshavardhan Kulkarni',
-      email: 'harsh.kulkarni@mail.com',
-      image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    }  
-];
+import { Alert, Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={dummyData}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.card}>
-            <Image
-              style={styles.image}
-              source={{ uri: item.image }}
-            />
-            <Text style={styles.nameText}>{item.name}</Text>
-            <Text style={styles.emailText}>{item.email}</Text>
-          </View>
-        )}
-        ItemSeparatorComponent={() => <View style={styles.separator} />}
-        numColumns={2}
+      <Text style={{ textAlign: 'center', fontSize:26, fontWeight:'bold' }}>Welcome again</Text>
+      <Text style={{ textAlign: 'center', fontSize:16, fontWeight:'bold' }}>LOGIN !!</Text>
+      <TextInput
+        type="text"
+        style={styles.textInput}
+        placeholder='Username'
       />
+      <TextInput
+        type="text"
+        style={styles.textInput}
+        placeholder='Password'
+      />
+      <Pressable 
+        style={{ width: '100%', backgroundColor: '#8174A0', padding: 10, borderRadius: 5, alignItems: 'center', marginTop: 10 }}
+        onPress={() => Alert.alert('Login')}
+        >
+          <Text>Login</Text>
+        </Pressable>
+
     </View>
-  );
-};
+  )
+}
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-  },
-  card: {
-    width:'30%',
-    height:'100%',
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 15,
+    backgroundColor: 'white',
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    marginHorizontal: 10,
+    justifyContent: 'center',
+    padding: 10,
   },
-  image: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-    marginBottom: 10,
-  },
-  nameText: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-  emailText: {
-    fontSize: 14,
-    color: '#777',
-    marginTop: 5,
-  },
-  separator: {
-    height: 15,
-  },
-});
-
-export default App;
+  textInput: {
+    backgroundColor: '#dada',
+    width: '100%',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    borderBottomWidth: 2,
+    borderBottomColor: '#000',
+  }
+})

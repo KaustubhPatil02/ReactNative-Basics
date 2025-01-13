ae22fe558fac6b3014e09bcb502d370ef729d613
Kaustubh Patil <patilkaustubh990@gmail.com>
2025-01-13 17:16:00 +0530

    Implement login functionality with username and password input fields

--- App.tsx
+++ App.tsx
@@ -1,27 +1,41 @@
 import { Alert, Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+
   return (
     <View style={styles.container}>
       <Text style={{ textAlign: 'center', fontSize:26, fontWeight:'bold' }}>Welcome again</Text>
       <Text style={{ textAlign: 'center', fontSize:16, fontWeight:'bold' }}>LOGIN !!</Text>
       <TextInput
-        type="text"
         style={styles.textInput}
         placeholder='Username'
+        value={username}
+        onChangeText={setUsername}
       />
       <TextInput
-        type="text"
         style={styles.textInput}
         placeholder='Password'
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
       />
       <Pressable 
         style={{ width: '100%', backgroundColor: '#8174A0', padding: 10, borderRadius: 5, alignItems: 'center', marginTop: 10 }}
-        onPress={() => Alert.alert('Login')}
+        onPress={() => Alert.alert('Login', `Username: ${username}, Password: ${password}`, [{ text: 'Close' }])}
         >
-          <Text>Login</Text>
+          <Text style={{ color: 'white' }}>Login</Text>
         </Pressable>
+        <View style={{ marginTop: 20 }}>
+          <Text style={{fontSize: 20,  fontWeight:'bold'}}>Your Entered Data for handling users input:</Text>
+          <Text style={{fontSize: 16}}>Username:{username} </Text>
+          <Text style={{fontSize: 16}}>Password:{password} </Text>
+
+        </View>
 
     </View>
   )
